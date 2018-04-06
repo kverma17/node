60269535f51509b719af9b33a8992f7033ecc8ba
Karan Verma <you@example.com>
2018-04-06 17:22:40 +0530

    post with html

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const data = {
+    "users":[{
+        "user":"admin",
+        "pass":"password",
+        "id":1
+    },{
+        "user":"admin1",
+        "pass":"password1",
+        "id":2
+    }
+    ]
+}
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -11,8 +23,11 @@ app.get('/users',function(req,res){
     res.send('GET SEND');
 })
 app.post('/users',function(req,res){
+    req.body.id = data.users.length + 1;
     console.log(req.body);
+    data.users.push(req.body);
     res.send('POST SEND');
+    console.log(data);
 })
 app.get('/users/:id',function(req,res){
     console.log(req.params);
@@ -28,3 +43,4 @@ app.delete('/users/:id',function(req,res){
 })
 
 app.listen(3000);
+console.log('Server running at localhost:3000');
